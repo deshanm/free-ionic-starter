c0f0dbf369ebdf600b16b40eacf50be1ad064212
Deshan Madurajith <madurajithdeshan@gmail.com>
2016-12-22 21:57:10 +0530

    cleaned dashboard ...

--- www/js/services.js
+++ www/js/services.js
@@ -5,29 +5,46 @@ angular.module('starter.services', [])
 
   // Some fake testing data
   var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/icons/fb.svg'
-  }, {
     id: 1,
     name: 'Max Lynx',
+    online: true,
+    msgCount: 5,
+    time: '11.35 AM',
     lastText: 'Hey, it\'s me',
     face: 'img/max.png'
   }, {
     id: 2,
     name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
+    msgCount: 1,
+    time: '10.35 AM',
+
+    lastText: 'So careful with our relationship. Now you are part of us.',
     face: 'img/adam.jpg'
   }, {
     id: 3,
+    time: 'YESTERDAY',
+
     name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
+    online: true,
+
+    lastText: 'You have mistaken I am not the person whom u r looking for!',
     face: 'img/perry.png'
   }, {
     id: 4,
+    time: 'YESTERDAY',
+
     name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
+    lastText: 'But I still have that voucher for mc donalds! What about that instead?',
+    face: 'img/mike.png'
+  },
+  {
+    id: 5,
+    msgCount: 2,
+    time: 'FRIDAY',
+
+    name: 'Alice Whitman',
+    online: true,
+    lastText: 'Thank You hunny. I love u sooooo much......',
     face: 'img/mike.png'
   }];
 
